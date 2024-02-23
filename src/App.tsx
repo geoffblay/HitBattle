7dc176ed83a3f9228e8c4d59bb8d8815d6a5fcb6
Geoff Blaylock <gblaylock2024@gmail.com>
2024-02-23 14:54:54 -0800

    home setup with routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
     return (
         <BrowserRouter>
             <Navbar />
             <Routes>
+                <Route path="/" element={<Home />} />
             </Routes>
         </BrowserRouter>
     )
