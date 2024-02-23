d38fdd38ac4f46304d8bcb1e8947f206f3d583c2
Geoff Blaylock <gblaylock2024@gmail.com>
2024-02-23 14:42:22 -0800

    homepage setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
-import { useState } from 'react'
 import Navbar from './components/Navbar'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
-    const [count, setCount] = useState(0)
-
     return (
-        <>
-            <div>
-                <Router>
-                    <Navbar />
-                </Router>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
