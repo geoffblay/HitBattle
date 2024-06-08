45279083e4931cd231b752ec10c210cabc207545
Geoff Blaylock <gblaylock2024@gmail.com>
2024-06-07 19:26:17 -0700

    popup working

--- src/components/ResultList.tsx
+++ src/components/ResultList.tsx
@@ -1,33 +1,76 @@
 import styled from "styled-components"
 import ResultsOverlap from "./ResultsOverlap";
 import { Battle } from "../types";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { useEffect, useState } from "react";
+import { getBattle } from "../firebase/GetBattle";
+import ArtistBox from "./ArtistBox";
 
 interface ResultListProps {
     battles: Battle[];
     limit?: number;
-    onBattleSelect: (battle: Battle) => void;
 }
 
-const ResultList = ({ battles, limit, onBattleSelect }: ResultListProps) => {
-  return (
-    <ResultListContainer>
-        {battles.slice(0, limit).map((battle, index) => {
-            return (
-                <ResultContainer onClick={() => {
-                    onBattleSelect(battle); 
-                }}>
-                    <ResultsOverlap 
-                        key={index}
-                        winner={battle.winner}
-                        loser={battle.loser}
-                        height={100}
-                    />
-                    <Score>{battle.winnerScore} - {battle.loserScore}</Score>
-                </ResultContainer>
-            );
-        })}
-    </ResultListContainer>
-  )
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+const ResultList = ({ battles, limit }: ResultListProps) => {
+    const [open, setOpen] = useState(false);
+    const [selectedBattle, setSelectedBattle] = useState<Battle | null>(null);
+    const handleOpen = (battle: Battle) => {
+        setOpen(true);
+        setSelectedBattle(battle);
+    }
+    const handleClose = () => setOpen(false);
+
+    useEffect(() => {
+        if (selectedBattle) {
+            getBattle(selectedBattle.id).then(battle => {
+                setSelectedBattle(battle || null);
+            });
+        }
+    }, [selectedBattle]);
+
+    return (
+        <ResultListContainer>
+            {battles.slice(0, limit).map((battle, index) => {
+                return (
+                    <>
+                        <ResultContainer onClick={() => handleOpen(battle)}>
+                            <ResultsOverlap
+                                key={index}
+                                winner={battle.winner}
+                                loser={battle.loser}
+                                height={100}
+                            />
+                            <Score>{battle.winnerScore} - {battle.loserScore}</Score>
+                        </ResultContainer>
+                        <Modal
+                            open={open}
+                            onClose={handleClose}
+                            aria-labelledby="modal-modal-title"
+                            aria-describedby="modal-modal-description"
+                        >
+                            <Box sx={style}>
+                                fart
+                            </Box>
+                        </Modal>
+                    </>
+                );
+            })}
+        </ResultListContainer>
+    )
 }
 
 export default ResultList
